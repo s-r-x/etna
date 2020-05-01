6d6547225e8b9cbaa56fa3c441a219d2e4843f27
srx <strax1990@gmail.com>
2020-05-01 20:28:48 +0500

    update antd

--- src/layouts/Root/index.tsx
+++ src/layouts/Root/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import cls from "./index.less";
 import { Layout, Menu, Typography } from "antd";
+import { HomeOutlined, SettingOutlined } from "@ant-design/icons";
 
 const { Header, Content, Sider } = Layout;
-const { SubMenu } = Menu;
 
 export default class RootLayout extends React.Component {
   state = {
-    collapsed: false,
+    collapsed: true,
   };
 
   onCollapse = (collapsed: boolean) => {
@@ -28,36 +28,12 @@ export default class RootLayout extends React.Component {
             </Typography.Title>
           </div>
           <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-            <Menu.Item key="1">
-              <span>Option 1</span>
+            <Menu.Item icon={<HomeOutlined />} key="1">
+              <span>Home</span>
             </Menu.Item>
-            <Menu.Item key="2">
-              <span>Option 2</span>
+            <Menu.Item icon={<SettingOutlined />} key="2">
+              <span>Settings</span>
             </Menu.Item>
-            <SubMenu
-              key="sub1"
-              title={
-                <span>
-                  <span>User</span>
-                </span>
-              }
-            >
-              <Menu.Item key="3">Tom</Menu.Item>
-              <Menu.Item key="4">Bill</Menu.Item>
-              <Menu.Item key="5">Alex</Menu.Item>
-            </SubMenu>
-            <SubMenu
-              key="sub2"
-              title={
-                <span>
-                  <span>Team</span>
-                </span>
-              }
-            >
-              <Menu.Item key="6">Team 1</Menu.Item>
-              <Menu.Item key="8">Team 2</Menu.Item>
-            </SubMenu>
-            <Menu.Item key="9"></Menu.Item>
           </Menu>
         </Sider>
         <Layout>
