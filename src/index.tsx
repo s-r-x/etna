dc0de8ced8b47699302b90693f13c1146bef79c4
srx <strax1990@gmail.com>
2020-11-10 14:13:49 +0500

    react-persist dep / persist the store

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "@/containers/App.tsx";
 import { Provider } from "react-redux";
-import store from "@/store";
+import store, { persistor } from "@/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { Spin } from "antd";
 
 if (module.hot) module.hot.accept();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={<Spin size="large" />} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,20 +2,40 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "./rootReducer";
 import rootSaga from "./rootSaga";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const sagaMiddleware = createSagaMiddleware();
 
 const middleware = [sagaMiddleware];
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: [
-    ,
     ...getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
     }),
     ...middleware,
   ],
 });
+export const persistor = persistStore(store);
 if (process.env.NODE_ENV !== "production" && module.hot) {
   module.hot.accept("./rootReducer", () => store.replaceReducer(rootReducer));
 }
