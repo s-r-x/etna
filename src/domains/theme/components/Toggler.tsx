6f2140a754b03837ca8d6d1c6dddc4f6e57cb1d9
srx <strax1990@gmail.com>
2020-11-19 11:02:13 +0500

    bump antd / theme toggler icons

--- src/domains/theme/components/Toggler.tsx
+++ src/domains/theme/components/Toggler.tsx
@@ -3,14 +3,29 @@ import { Switch } from "antd";
 import { useSelector, useDispatch } from "react-redux";
 import { ThemeSelectors } from "../store/selectors";
 import { ThemeActions } from "../store/slice";
+import MoonIcon from "@/components/icons/Moon";
+import SunIcon from "@/components/icons/Sun";
+import styled from "styled-components";
+
+const StyledSwitch = styled(Switch)`
+  && {
+    background-color: ${({ checked }) => (checked ? "#436185" : "#84AEFF")};
+  }
+`;
 
 const ThemeToggler = () => {
   const mode = useSelector(ThemeSelectors.getMode);
   const dispatch = useDispatch();
   return (
-    <Switch
+    <StyledSwitch
       checked={mode === "dark"}
       onChange={(v) => dispatch(ThemeActions.changeMode(v ? "dark" : "light"))}
+      checkedChildren={<MoonIcon style={{ color: "#fff" }} />}
+      unCheckedChildren={
+        <SunIcon
+          style={{ fontSize: "16px", paddingTop: "3px", color: "#FEB356" }}
+        />
+      }
     />
   );
 };
