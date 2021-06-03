f9e99757e127c178945f3d8eaae1d7c3ca4c9b12
s-r-x <strax1990@gmail.com>
2021-06-03 12:33:29 +0500

    wrap phoenix/socketio pages in TwoColResizable layout

--- app/src/pages/phoenix/index.tsx
+++ app/src/pages/phoenix/index.tsx
@@ -1,19 +1,10 @@
 import React from "react";
-import { Row, Col } from "antd";
 import PhoenixModule from "@/domains/ws/phoenix/components";
 import PhoenixLogger from "@phoenix/components/Logger";
+import TwoColResizable from "@/layouts/TwoColResizable";
 
 export const PhoenixPage = () => {
-  return (
-    <Row style={{ flex: 1 }} gutter={12}>
-      <Col span={12}>
-        <PhoenixModule />
-      </Col>
-      <Col span={12}>
-        <PhoenixLogger />
-      </Col>
-    </Row>
-  );
+  return <TwoColResizable left={<PhoenixModule />} right={<PhoenixLogger />} />;
 };
 
 export default PhoenixPage;
--- app/src/pages/socketio/index.tsx
+++ app/src/pages/socketio/index.tsx
@@ -1,20 +1,11 @@
 import React from "react";
-import { Row, Col } from "antd";
 import SocketIoRequest from "@/domains/ws/socket-io/components";
 import SocketIOLogger from "@socket-io/components/Logger";
+import TwoColResizable from "@/layouts/TwoColResizable";
 
 export const SocketIOPage = () => {
   return (
-    <>
-      <Row style={{ flex: 1 }} gutter={12}>
-        <Col span={12}>
-          <SocketIoRequest />
-        </Col>
-        <Col span={12}>
-          <SocketIOLogger />
-        </Col>
-      </Row>
-    </>
+    <TwoColResizable left={<SocketIoRequest />} right={<SocketIOLogger />} />
   );
 };
 
