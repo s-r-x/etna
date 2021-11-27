ad8bddd0f453b900b0def887e5f5eee00f36decf
s-r-x <strax1990@gmail.com>
2021-11-27 22:53:58 +0500

    clear socketio phx message after hitting send button

--- app/src/domains/ws/phoenix/store/sagas/message.ts
+++ app/src/domains/ws/phoenix/store/sagas/message.ts
@@ -1,11 +1,12 @@
 import { SagaIterator } from "redux-saga";
-import { takeLatest, call, select } from "typed-redux-saga";
+import { takeLatest, call, select, put } from "typed-redux-saga";
 import { PhoenixSelectors as Selectors } from "../selectors";
 import { PhoenixActions as Actions } from "../slice";
 
 function* messageSaga(): SagaIterator {
   const message = yield* select(Selectors.getSendMessageDto);
   const client = yield* select(Selectors.getClient);
+  yield* put(Actions.changeInputData(""));
   yield* call(client.send, message);
 }
 export default function* watchSocketIoMessage() {
